93cf67f0e87044950f859e773a149ba7cc66ff1c
Arshia <126101336+arshiars@users.noreply.github.com>
2024-10-29 18:25:00 -0400

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -44,8 +44,6 @@ export default function FormBlock(props) {
             onSubmit={handleSubmit}
             ref={formRef}
             data-sb-field-path= {fieldPath}
-            method="POST" 
-            data-netlify="true"
         >
             <div
                 className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
