0063b19f25218ba854e8b689a6a5d00adbfc9ebd
Arshia <126101336+arshiars@users.noreply.github.com>
2024-10-29 18:15:13 -0400

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -22,53 +22,74 @@ export default function FormBlock(props) {
     }
 
     return (
-        <form
-            className={classNames(
-                'sb-component',
-                'sb-component-block',
-                'sb-component-form-block',
-                className,
-                styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
-                styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
-                styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
-                    ? mapStyles({
-                          borderWidth: styles?.self?.borderWidth,
-                          borderStyle: styles?.self?.borderStyle,
-                          borderColor: styles?.self?.borderColor ?? 'border-primary'
-                      })
-                    : undefined,
-                styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
-            )}
-            name={elementId}
-            id={elementId}
-            onSubmit={handleSubmit}
-            ref={formRef}
-            data-sb-field-path= {fieldPath}
-            method="POST" 
-            data-netlify="true"
-        >
-            <div
-                className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
-                {...(fieldPath && { 'data-sb-field-path': '.fields' })}
-            >
-                <input type="hidden" name="form-name" value={elementId} />
-                {fields.map((field, index) => {
-                    const modelName = field.__metadata.modelName;
-                    if (!modelName) {
-                        throw new Error(`form field does not have the 'modelName' property`);
-                    }
-                    const FormControl = getComponent(modelName);
-                    if (!FormControl) {
-                        throw new Error(`no component matching the form field model name: ${modelName}`);
-                    }
-                    return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
-                })}
-            </div>
-            {submitButton && (
-                <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
-                    <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
-                </div>
-            )}
+        // <form
+        //     className={classNames(
+        //         'sb-component',
+        //         'sb-component-block',
+        //         'sb-component-form-block',
+        //         className,
+        //         styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
+        //         styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
+        //         styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
+        //             ? mapStyles({
+        //                   borderWidth: styles?.self?.borderWidth,
+        //                   borderStyle: styles?.self?.borderStyle,
+        //                   borderColor: styles?.self?.borderColor ?? 'border-primary'
+        //               })
+        //             : undefined,
+        //         styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
+        //     )}
+        //     name={elementId}
+        //     id={elementId}
+        //     onSubmit={handleSubmit}
+        //     ref={formRef}
+        //     data-sb-field-path= {fieldPath}
+        //     method="POST" 
+        //     data-netlify="true"
+        // >
+        //     <div
+        //         className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
+        //         {...(fieldPath && { 'data-sb-field-path': '.fields' })}
+        //     >
+        //         <input type="hidden" name="form-name" value={elementId} />
+        //         {fields.map((field, index) => {
+        //             const modelName = field.__metadata.modelName;
+        //             if (!modelName) {
+        //                 throw new Error(`form field does not have the 'modelName' property`);
+        //             }
+        //             const FormControl = getComponent(modelName);
+        //             if (!FormControl) {
+        //                 throw new Error(`no component matching the form field model name: ${modelName}`);
+        //             }
+        //             return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
+        //         })}
+        //     </div>
+        //     {submitButton && (
+        //         <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
+        //             <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
+        //         </div>
+        //     )}
+        // </form>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
+
     );
 }
