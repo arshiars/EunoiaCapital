dd4229a5fd80adf34e88f2cde90cee19850fc33d
Arshia <126101336+arshiars@users.noreply.github.com>
2024-10-30 16:16:49 +0000

    Add radius

--- sources/local/models/ImageBlock.ts
+++ sources/local/models/ImageBlock.ts
@@ -77,7 +77,7 @@ export const ImageBlock: Model = {
                             color: '$primary'
                         }
                     ],
-                    borderRadius: '10'
+                    borderRadius: '*'
                 }
             }
         }
