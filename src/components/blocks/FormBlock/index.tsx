40aa3ec43ec06efcd49ada3a0eb746ce852c695a
Arshia <126101336+arshiars@users.noreply.github.com>
2024-10-30 14:20:20 +0000

    update form

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -42,9 +42,9 @@ export default function FormBlock(props) {
             )}
             data-netlify={true}
             data-netlify-recaptcha={true}
-            name={contact}
-            // id={elementId}
-            onSubmit={"/thank you"}
+            // name={contact}
+            id={elementId}
+            // onSubmit={"/thank you"}
             // ref={formRef}
             // data-sb-field-path={fieldPath}
 
